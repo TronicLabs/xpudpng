a2b484ebd6fced03de76ed2c98252cf062d83d6d
Marc Lagrange <rhaamo@gruik.at>
2010-12-10 23:27:36 +0100

    The plugin can receive packets without blocking the simulator (unless we receive a giant flow of packets, but we should not because the client->plugin is only for registering the client and datasets)

--- plugin/main.c
+++ plugin/main.c
@@ -20,26 +20,55 @@ XPWidgetID AboutWindow = NULL;
 XPWidgetID AboutTextWidget[50] = {
 	NULL
 };
+XPLMMenuID PluginMenu;
+int PluginSubMenuItem;
 
 void AboutMenuHandler(void *, void *);
 void CreateAboutWidget(int x1, int y1, int w, int h);
 int AboutHandler(XPWidgetMessage inMessage, XPWidgetID inWidget, long iParam1, long inParam2);
+void create_menu(void);
+
+/* Socket Stuff */
+void set_addresses(void);
+int ensure_socket_bound(void);
+
+int plugin_enabled;
+int send_enabled;
+int recv_enabled;
+int recv_socket_open;
+
+int receiverSocket;
+
+struct sockaddr_in receiverSocketSrcAddr;
+
+fd_set receiverReadfs;
+
+float ReceiveClientTalkCallback(float inElapsedSinceLastCall, float inElapsedTimeSinceLastFlightLoop,
+    int inCounter, void *inRefcon);
+
 
 PLUGIN_API int XPluginStart(char *outName, char *outSig, char *outDesc)
 {
-	XPLMMenuID PluginMenu;
-	int PluginSubMenuItem;
+
 
 	strcpy(outName, "XPUDPng " VERSION);
-	strcpy(outSig, "xpudpng.rhaamo.li");
-	strcpy(outDesc, "Send choosed DataRefs values to registered clients");
+	strcpy(outSig, "li.rhaamo.xpudpng");
+	strcpy(outDesc, "Send simuator data to an XPUDPNG-compliant application using UDP.");
+
+	/* Initialize status variables */
+	plugin_enabled = 0; /* set to 1 if the plugin is enabled from the menu. 0 otherwise */
+	send_enabled = 1;   /* set to 1 if socket is open and preferences window is not shown. 0 otherwise */
+	recv_enabled = 1;   /* set to 1 if socket is open and preferences window is not shown. 0 otherwise */
+	recv_socket_open = 0;
 
 	XPLMDebugString("XPUDPNG: Plugin " VERSION "\n");
 
-	/* Create the Menu */
-	PluginSubMenuItem = XPLMAppendMenuItem(XPLMFindPluginsMenu(), "XPUDPng", NULL, 1);
-	PluginMenu = XPLMCreateMenu("XPUDPng " VERSION, XPLMFindPluginsMenu(), PluginSubMenuItem, AboutMenuHandler, NULL);
-	XPLMAppendMenuItem(PluginMenu, "About XPUDPNG", (void *)+1, 1);
+
+	/* Create socket and addresses */
+	receiverSocket = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP);
+
+	(void)set_addresses();
+	(void)create_menu();
 
 	/* Flag to tell us if the widget is being displayed */
 	gMenuItem = 0;
@@ -53,15 +82,37 @@ PLUGIN_API void XPluginStop(void)
 		    XPDestroyWidget(AboutWidget, 1);
 		    gMenuItem = 0;
 	    }
+	if (recv_socket_open == 1) {
+		if (close(receiverSocket) == -1) {
+			XPLMDebugString("XPUDPNG: failed - Caught error while closing socket!(");
+			XPLMDebugString((char * const)strerror(errno));
+			XPLMDebugString(")\n");
+		} else {
+			XPLMDebugString("XPUDPNG: Closed socket\n");
+		}
+		recv_socket_open = 0;
+		recv_enabled = 0;
+	}
 }
 
 PLUGIN_API int XPluginEnable(void)
 {
-	return 1;
+	recv_enabled = 1;
+	plugin_enabled = 1;
+	XPLMDebugString("XPUDPNG: enabled\n");
+
+	/* Register Flight Loop Callbacks */
+	XPLMRegisterFlightLoopCallback(ReceiveClientTalkCallback, XPUDPNG_RECV_FREQ_CALL, NULL);
+
+	return ensure_socket_bound();
 }
 
 PLUGIN_API int XPluginDisable(void)
 {
+	XPLMUnregisterFlightLoopCallback(ReceiveClientTalkCallback, NULL);
+	XPLMDebugString("XPUDPNG: disabled\n");
+	plugin_enabled = 0;
+	recv_enabled = 0;
 	return 1;
 }
 
@@ -135,3 +186,76 @@ int AboutHandler(XPWidgetMessage inMessage, XPWidgetID inWidget, long inParam1, 
 	return 0;
 }
 
+/* Create the Menu */
+void create_menu(void)
+{
+	PluginSubMenuItem = XPLMAppendMenuItem(XPLMFindPluginsMenu(), "XPUDPng", NULL, 1);
+	PluginMenu = XPLMCreateMenu("XPUDPng " VERSION, XPLMFindPluginsMenu(), PluginSubMenuItem, AboutMenuHandler, NULL);
+	XPLMAppendMenuItem(PluginMenu, "About XPUDPNG", (void *)+1, 1);
+}
+
+void set_addresses(void)
+{
+	XPLMDebugString("XPUDPNG: Setting addresses\n");
+
+	receiverSocketSrcAddr.sin_family = AF_INET;
+	receiverSocketSrcAddr.sin_port = htons(XPUDPNG_RECV_PORT);
+	receiverSocketSrcAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+	char for_port[100];
+	sprintf(for_port, "XPUDPNG: Prefs: IP=any, RECV-PORT=%i\n", XPUDPNG_RECV_PORT);
+	XPLMDebugString(for_port);
+}
+
+int ensure_socket_bound(void)
+{
+	if (recv_socket_open != 1) {
+		if (bind(receiverSocket, (struct sockaddr*)&receiverSocketSrcAddr, sizeof(struct sockaddr)) == -1) {
+			XPLMDebugString("XPUDPNG: caught error while binding socket (");
+			XPLMDebugString((char * const)strerror(errno));
+			XPLMDebugString(")\n");
+			recv_socket_open = 0;
+			return -1;
+		} else {
+			XPLMDebugString("XPUDPNG: Socket bound!\n");
+			recv_socket_open = 1;
+			return 1;
+		}
+	} else {
+		return 1;
+	}
+}
+
+float
+ReceiveClientTalkCallback(float inElapsedSinceLastCall, float inElapsedTimeSinceLastFlightLoop,
+    int inCounter, void *inRefcon)
+{
+	if ((plugin_enabled == 1) && (recv_enabled == 1) && (recv_socket_open == 1)) {
+		char buffer[256];
+		int ret = 0;
+		struct timeval nowait;
+		nowait.tv_sec = 0;
+		nowait.tv_usec = 0; /* nowait please */
+		FD_ZERO(&receiverReadfs);
+		FD_SET(receiverSocket, &receiverReadfs);
+		if ((ret = select(receiverSocket + 1, &receiverReadfs, NULL, NULL, &nowait)) < 0) {
+			XPLMDebugString("XPUDPNG: caught error while select (");
+			XPLMDebugString((char * const)strerror(errno));
+			XPLMDebugString(")\n");
+			return -1;
+		}
+
+		if (ret == 0) {
+		}
+
+		if (FD_ISSET(receiverSocket, &receiverReadfs))
+		    {
+			    recv(receiverSocket, buffer, sizeof(buffer), 0);
+			    XPLMDebugString("XPUDPNG: Got input packet\n");
+			    return XPUDPNG_RECV_FREQ_RETURN;
+		    }
+		return XPUDPNG_RECV_FREQ_RETURN;
+	} else {
+		return 1;
+	}
+}
--- plugin/main.h
+++ plugin/main.h
@@ -14,4 +14,23 @@
 #include <stdlib.h>
 #include <string.h>
 
+#if IBM
+#warning "UNTESTED !"
+#include <windows.h>
+#else /* IBM */
+#include <sys/socket.h>
+#include <sys/errno.h>
+#include <sys/types.h>
+#include <sys/select.h>
+#include <sys/time.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#endif /* IBM */
+
+#define XPUDPNG_SEND_PORT 49020
+#define XPUDPNG_RECV_PORT 49021
+#define XPUDPNG_RECV_FREQ_CALL 0.01
+#define XPUDPNG_RECV_FREQ_RETURN 0.016 // 60hz
+
 #endif /* _MAIN_H */
